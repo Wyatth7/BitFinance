2adab3cf2721fdd17c01bc21f50ddae248630a31
victoria Adane <victoriaadane2@gmail.com>
2023-09-23 16:24:16 -0500

    add createUser firebase function

--- src/app/@pages/authentication/components/sign-up/sign-up.component.ts
+++ src/app/@pages/authentication/components/sign-up/sign-up.component.ts
@@ -1,7 +1,10 @@
 import { AfterViewChecked, AfterViewInit, Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserRole } from 'functions/src/shared/enums/user-role';
+import { CreateUserModel } from 'src/app/shared/models/users/create-user-model';
 import { passwordValidator, passwordsMatchValidator } from 'src/app/shared/form/validators/password-validator';
 import { AuthenticationFormModel } from 'src/app/shared/models/members/form/authentication-form-model';
+import { UserService } from 'src/app/shared/services/user/user.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,7 +12,8 @@ import { AuthenticationFormModel } from 'src/app/shared/models/members/form/auth
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
-  constructor(private formBulider: FormBuilder) {
+  constructor(private UserService: UserService, 
+    private formBulider: FormBuilder) {
     this.formControls.get('passwords')?.setValidators(passwordsMatchValidator())
   }
 
@@ -49,6 +53,20 @@ export class SignUpComponent {
 
   async signUpAction(): Promise<void> {
     console.log(this.formControls);
+
+    const controls = this.formControls.value;
+
+    const createUserModel: CreateUserModel = {
+      firstName: controls.name?.firstName!,
+      lastName: controls.name?.lastName!,
+      email: controls.email!,
+      password: controls.passwords?.password!,
+      role: +UserRole.user,
+      requested: true,
+      securityQuestionAnswer: new Date()
+    }
+
+    await this.UserService.createUser(createUserModel);
   }
 
   get passwordControls():boolean {
