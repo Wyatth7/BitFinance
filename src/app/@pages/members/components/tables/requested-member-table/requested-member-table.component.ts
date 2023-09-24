83b827cda966fac1137d2927fbc3c8774ff028ed
wyatth7 <whardin1010@gmail.com>
2023-09-24 01:05:52 -0400

    Added accept deny users

--- src/app/@pages/members/components/tables/requested-member-table/requested-member-table.component.ts
+++ src/app/@pages/members/components/tables/requested-member-table/requested-member-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MemberModel } from 'src/app/shared/models/members/member-model';
 import { UserModel } from 'src/app/shared/models/users/user-model';
+import { UserService } from 'src/app/shared/services/user/user.service';
 
 @Component({
   selector: 'app-requested-member-table',
@@ -11,12 +12,14 @@ export class RequestedMemberTableComponent {
   @Input() userData!: UserModel[]
   displayedColumns: string[] = ['actions', 'name', 'username', 'email'] 
 
-  acceptUser() {
-    console.log('user accepted');
+  constructor(private userService: UserService) {}
+
+  async acceptUser(userId: string) {
+    await this.userService.acceptDenyUser(userId, true)
   }
 
-  declineUser() {
+  async declineUser(userId: string) {
     console.log('user rejected');
-    
+    await this.userService.acceptDenyUser(userId);
   }
 }
