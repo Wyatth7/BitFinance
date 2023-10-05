2cc6869ad70510ca324ade49858f50b678e6742a
wyatth7 <whardin1010@gmail.com>
2023-10-05 17:54:03 -0400

    Fixed accounts not loading after create

--- src/app/shared/components/tables/table/table.component.ts
+++ src/app/shared/components/tables/table/table.component.ts
@@ -1,5 +1,5 @@
 import { SelectionModel } from '@angular/cdk/collections';
-import { AfterContentInit, AfterViewInit, Component, ContentChildren, Input, OnInit, QueryList, ViewChild } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ContentChildren, Input, OnChanges, OnInit, QueryList, SimpleChanges, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatColumnDef, MatTable, MatTableDataSource } from '@angular/material/table';
 
@@ -14,7 +14,7 @@ import { MatColumnDef, MatTable, MatTableDataSource } from '@angular/material/ta
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent<T> implements OnInit, AfterContentInit {
+export class TableComponent<T> implements OnInit, OnChanges, AfterContentInit {
   @Input() tableData!: T[];
   @Input() displayedColumns!: string[];
   @Input() 
@@ -32,6 +32,27 @@ export class TableComponent<T> implements OnInit, AfterContentInit {
   ngOnInit(): void {
       this.displayedColumns.unshift('select')
       this.dataSource = new MatTableDataSource(this.tableData);
+  }
+
+  /**
+   * Used to update table when accounts added
+   * @param changes simple chagnes
+   */
+  ngOnChanges(changes: SimpleChanges): void {
+    // do nothing if data source is null
+    if (!this.dataSource) return;
+
+    // do nothing if search filter is currently applied
+    if (this.dataSource.filter.length > 0) return;
+    
+    // create new table data source
+    this.dataSource = new MatTableDataSource(this.tableData)
+
+    if (this.sort) {
+      this.sort.forEach(sort => {
+        this.dataSource.sort = sort
+      })
+    }
   }
 
   ngAfterContentInit(): void {
@@ -54,13 +75,12 @@ export class TableComponent<T> implements OnInit, AfterContentInit {
 
   applyFilter(value: string | string[] | undefined) {
     if (!this.dataSource) return;
-
+    
     if (!value) {
       this.dataSource.filter = '';
       return;
     };
     
-
     if (Array.isArray(value)) {
       this.dataSource.filter = value
         .join('+')
