5f8d56893cb014963a0e6ca6d3cdcd1835b5202a
wyatth7 <whardin1010@gmail.com>
2023-10-11 17:09:12 -0400

    Connected accounts to event log

--- functions/src/shared/helpers/log/event-logger.ts
+++ functions/src/shared/helpers/log/event-logger.ts
@@ -2,28 +2,47 @@ import * as logger from 'firebase-functions/logger'
 import * as admin from 'firebase-admin';
 import { FirestoreCollections } from "../../enums/firestore-collections";
 import { EventLogModel } from '../../models/event-log/event-log-model';
+import { Guid } from '../guids/generate-guid';
 
 export abstract class EventLogger {
 
+    /**
+     * Creates an event in the event logger collection
+     * @param eventData Event data model
+     */
     public static async createEventLog(eventData: EventLogModel) {
-        if (!eventData)
-            logger.warn("Trying to save null EventData {}", eventData);
+        if (!eventData){
+            logger.warn(`Trying to save null EventData ${eventData}`);
             return;
+        }
+
+        eventData.eventLogId = Guid.createGuid();
+        eventData.dateChanged = new Date().toISOString();
+
         try {
-            await admin.firestore().collection(FirestoreCollections.eventLogs).doc().set(eventData);
+            await admin.firestore()
+                .collection(FirestoreCollections.eventLogs)
+                .doc(eventData.eventLogId)
+                .set(eventData);
         } catch (error) {
             logger.error(error);
         }
     }
 
-    public static async getEventLog(eventLogId: string) { 
+    /**
+     * Returns a single event log
+     * @param hostId Id of the event log host
+     * @returns Event log model
+     */
+    public static async getEventLog(hostId: string) { 
         try {
             const eventLogSnapshot = await admin.firestore()
-                            .collection(FirestoreCollections.eventLogs.valueOf().toString())
-                            .where('uid', '==', eventLogId).get();
+                            .collection(FirestoreCollections.eventLogs)
+                            .where('hostId', '==', hostId)
+                            .get();
 
             if (eventLogSnapshot.empty) {
-                logger.error("EventData with id {} is not found.", eventLogId);
+                logger.error("EventData with id {} is not found.", hostId);
                 return null;
             }
 
@@ -40,17 +59,21 @@ export abstract class EventLogger {
         }
     }
 
+    /**
+     * Gets a list of event logs based on host ID
+     * @returns List of event log models
+     */
     public static async getEventLogList() {
         try {
             const eventLogSnapshot = await admin.firestore()
-                                    .collection(FirestoreCollections.users.valueOf().toString())
+                                    .collection(FirestoreCollections.eventLogs)
                                     .get();
 
             if (eventLogSnapshot.empty) {
                 return null;
             }
 
-            let eventLogList: EventLogModel[] | EventLogModel[] = new Array();
+            let eventLogList: EventLogModel[] = [];
             
             eventLogSnapshot.forEach(eventLogDoc => {
                 const eventLog = eventLogDoc.data() as EventLogModel;
