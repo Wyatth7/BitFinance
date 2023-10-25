28ee1bbb13715e45ef65ba287275ed8e21cbbbd5
wyatth7 <whardin1010@gmail.com>
2023-10-24 21:15:14 -0400

    Added more to single journal page

--- src/app/@pages/journals/single-view/single-view.component.ts
+++ src/app/@pages/journals/single-view/single-view.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatChipListboxChange } from '@angular/material/chips';
+import { GetEnumValueService } from 'src/app/shared/services/enum/get-enum-value.service';
 
 @Component({
   selector: 'app-single-view',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./single-view.component.scss']
 })
 export class SingleViewComponent {
+  date = new Date();
+  showTransactions = true;
 
+  displayedColumns = ['account', 'debit', 'credit'];
+  
+  files = [
+    {
+      accountName: 'Cash',
+      debit: 500,
+      credit: 500
+    }
+  ]
+  entries = [
+    {
+      accountName: 'Cash',
+      debit: 500,
+      credit: 0
+    },
+    {
+      accountName: 'Owner Equity',
+      debit: 0,
+      credit: 500
+    }
+  ]
+
+  tableViewTitle = 'Transactions';
+
+  constructor(public getEnum: GetEnumValueService) {}
+
+  change($event: MatChipListboxChange) {
+    if ($event.value === undefined) {
+      this.showTransactions = true;
+      this.tableViewTitle = 'Transactions';
+      return;
+    }
+
+      this.showTransactions = $event.value;
+
+      if (this.showTransactions) {
+        this.tableViewTitle = 'Transactions'
+        return;
+      }
+
+      this.tableViewTitle = 'Attached Files'
+  }
 }
