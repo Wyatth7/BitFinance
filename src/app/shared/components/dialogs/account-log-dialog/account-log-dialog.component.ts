5b3b9d20b5789a2135673e52eee9945118237856
Tom Porter <tporter6827@gmail.com>
2023-10-30 17:08:59 -0400

    Create modal/dialog box to view account event log

---
+++ src/app/shared/components/dialogs/account-log-dialog/account-log-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-log-dialog',
+  templateUrl: './account-log-dialog.component.html',
+  styleUrls: ['./account-log-dialog.component.scss']
+})
+export class AccountLogDialogComponent {
+
+}
