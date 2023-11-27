98fc6b0bef1f97db67ce895180f6189d350b1402
Tom Porter <tporter6827@gmail.com>
2023-11-27 18:33:28 -0500

    Additional acct sub-type.ts file created
    Updated account-model.ts files accordingly

---
+++ src/app/shared/enums/accounts/account-subtype.ts
@@ -0,0 +1,12 @@
+export enum AccountSubType {
+    cash = 11,
+    accountsReceivable = 12,
+    inventory = 13,
+    accountsPayable = 21,
+    shortTermDebt = 22,
+    unearnedRevenue = 23,
+    stock = 41,
+    retainedEarnings = 42,
+    ownersEquity = 43,
+    other = 51
+}
