39458323adb081e56d1952db8713715520ebbb1a
Tom Porter <tporter6827@gmail.com>
2023-11-28 21:25:02 -0500

    Fixed Merge Error
    Deleted Unnecessary Bits of Code

--- src/app/shared/enums/accounts/account-subtype.ts
+++ src/app/shared/enums/accounts/account-subtype.ts
@@ -1,5 +1,4 @@
 export enum AccountSubType {
-<<<<<<< HEAD
     currentAssets = 1,
     longTermAssets = 2,
     fixedAssets = 3,
@@ -19,16 +18,4 @@ export enum AccountSubType {
     treasuryEquity = 17,
     retainedEarnings = 18,
     other = 19
-=======
-    cash = 11,
-    accountsReceivable = 12,
-    inventory = 13,
-    accountsPayable = 21,
-    shortTermDebt = 22,
-    unearnedRevenue = 23,
-    stock = 41,
-    retainedEarnings = 42,
-    ownersEquity = 43,
-    other = 51
->>>>>>> 773292b5eccb4be437a78c580be70127639693d7
 }
